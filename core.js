85af79653dfff261bcf74a7f4850f6c26f2aea0d
Armando <mandy1339@gmail.com>
2017-08-22 18:46:01 -0400

    finished v1

--- core.js
+++ core.js
@@ -14,7 +14,11 @@ var request = require('request');           // http request support
 var mailer = require('nodemailer');         // email support
 var db = require('mysql');                  // mysql support
 
-console.log('\n-\n-\ntwitter config: ', twitconfig,'\n-\n-\n')
+var ethVal;             // Hold the value of eth in USD
+var btcVal;             // Hold the value of btc in USD
+var traders = [];       // This will be a vector of trader objects
+
+
 
 
 
@@ -80,7 +84,6 @@ var connection = db.createConnection({
   port:process.env.DB_PORT
 })
 
-console.log(process.env.DB_HOSTNAME);
 
 // // Example mysql usage
 // var queryAddTrader = `SELECT * FROM trader WHERE email = 'mandy1339@gmail.com';`;
@@ -95,24 +98,242 @@ console.log(process.env.DB_HOSTNAME);
 
 
 
+//REQUEST
+//
+//
+// request.get('http://192.168.1.5/api/3MWz5lzDLxmE3b-CEBB93hBgyiP2DuERszBLWjDf/groups/1', function (error, response, body){
+//     console.log('error:', error);
+//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//     console.log('body:', body); // Print the HTML for the Google homepage.
+//     hueNow = parseFloat(JSON.stringify(response.action.hue))
+// });
+
+
+
+
+
 // ENTRY POINT (MAIN)
 //==================================================================
 //==================================================================
 function scanAlert() {
-    console.log('ROUND # 1');
+   
+
+
+
+    // Get eth val
+    request.get('https://api.etherscan.io/api?module=stats&action=ethprice&apikey=YourApiKeyToken', function(error, response, body) {
+        if(error) {console.log('error: \n', error); return}
+        parsedBody = JSON.parse(body);
+        console.log('ETH: ', parsedBody.result.ethusd);
+        ethVal = parsedBody.result.ethusd;
+    });
+    
+    // Get btc Val
+    request.get('https://api.coinbase.com/v2/exchange-rates?currency=BTC', function(error, response, body) {
+        if(error) {console.log('error: \n', error); return}
+        parsedBody = JSON.parse(body);
+        console.log('BTC:', parsedBody.data.rates.USD);
+        btcVal = parsedBody.data.rates.USD;
+    });
+
+    // Get traders from db
+    var queryGetTraders = `SELECT * FROM trader;`;
+    connection.query(queryGetTraders, function(error, rows, columns) {
+        if(error) {console.log(error); return;}     //  stop if you get errors
+        traders = rows;                             // put traders in the array
+    })
+
+
+
+
+
+
+
+
+    // Perform scan
+    setTimeout(delayed, 1000);
+    function delayed() {
+        
+        traders.forEach(function(trader, index, array) {
+            if(trader.ethlow && ethVal < trader.ethlow) {
+                alertUserEthLow(trader);
+            }
+            else if(trader.ethhigh && ethVal > trader.ethhigh) {
+                alertUserEthHigh(trader);
+            }
+            if(trader.btclow && btcVal < trader.btclow) {
+                alertUserBtcLow(trader);
+            }
+            else if(trader.btchigh && btcVal > trader.btchigh) {
+                alertUserBtcHigh(trader);
+            }
+        })
+    }
+    
+
+
+
+
+
+// // Example mysql usage
+// var queryAddTrader = `SELECT * FROM trader WHERE email = 'mandy1339@gmail.com';`;
+// connection.query(queryAddTrader, function(error, rows, columns) {
+//     if(error){console.log(error); return;}; // if error return immediatelly
+//     console.log('\n\n\nquery result rows: ',rows);
+//     console.log('\n\n\nquery result columns: ',columns);
+// });
+
+
+
+
+
 }
 
-//setTimeout
-//setTimeout(scanAlert, 6000)
-scanAlert();
-
-// get the current eth and btc price
+setInterval(scanAlert, 60000)
 
 
 
-// query get all the rows
 
 
-// for every row, compare prices
+// ALERT FUNCTIONS
 
+// ALERT USER ETH LOW()
+function alertUserEthLow(trader) {
+    console.log('alerting user about eth low: ', trader);
+    if(trader.twit_acc) {       // twit user
+        
+    }
+    if(trader.phone) {          // sms user
+        
+    }
+    if(trader.cool == 'Y') {    // email user
+        // prepare email
+        var mail = {
+            from: "Crypto Alert <mandybot1339@gmail.com>",
+            to: trader.email,
+            subject:  'eth price down',
+            text: "Ethereum went down in price to your threshold of " + trader.ethlow + 'your threshold is being reset now',
+            html: "<b>Ethereum went down in price to your threshold of " + trader.ethlow + 'your threshold is being reset now</b>',
+        }
+        // send email
+        smtpTransport.sendMail(mail, function(error, response){
+            if(error) { console.log(error); }
+            else { console.log("Message sent to: " + response.accepted);}
+        });
 
+        // reset user ethlow to null
+        setTimeout(function () {
+            var queryReset = `UPDATE trader SET ethlow=NULL WHERE email='${trader.email}';`;
+            connection.query(queryReset, function(error, rows, columns) {
+                if(error) {console.log(error); return;}     //  stop if you get errors
+                console.log('rows: ', rows);
+            })
+
+        }, 5000)
+    }
+}
+
+// ALERT USER ETH HIGH()
+function alertUserEthHigh(trader) {
+    console.log('alerting user about eth high: ', trader);
+        if(trader.twit_acc) {       // twit user
+        
+    }
+    if(trader.phone) {          // sms user
+        
+    }
+    if(trader.cool == 'Y') {    // email user
+        // prepare email
+        var mail = {
+            from: "Crypto Alert <mandybot1339@gmail.com>",
+            to: trader.email,
+            subject:  'eth price up',
+            text: "Ethereum went up in price to your threshold of " + trader.ethhigh + 'your threshold is being reset now',
+            html: "<b>Ethereum went up in price to your threshold of " + trader.ethhigh + 'your threshold is being reset now</b>',
+        }
+        // send email
+        smtpTransport.sendMail(mail, function(error, response){
+            if(error) { console.log(error); }
+            else { console.log("Message sent to: " + response.accepted);}
+        });
+
+        // reset user ethhigh to null
+        setTimeout(function () {
+            var queryReset = `UPDATE trader SET ethhigh=NULL WHERE email='${trader.email}';`;
+            connection.query(queryReset, function(error, rows, columns) {
+                if(error) {console.log(error); return;}     //  stop if you get errors
+                console.log('rows: ', rows);
+            })
+        }, 5000)
+    }
+}
+
+// ALERT USER BTC LOW()
+function alertUserBtcLow(trader) {
+    console.log('alerting user about btc low: ', trader);
+    if(trader.twit_acc) {       // twit user
+        
+    }
+    if(trader.phone) {          // sms user
+        
+    }
+    if(trader.cool == 'Y') {    // email user
+        // prepare email
+        var mail = {
+            from: "Crypto Alert <mandybot1339@gmail.com>",
+            to: trader.email,
+            subject:  'btc price down',
+            text: "Bitcoin went down in price to your threshold of " + trader.btclow + 'your threshold is being reset now',
+            html: "<b>Bitcoin went down in price to your threshold of " + trader.btclow + 'your threshold is being reset now</b>',
+        }
+        // send email
+        smtpTransport.sendMail(mail, function(error, response){
+            if(error) { console.log(error); }
+            else { console.log("Message sent to: " + response.accepted);}
+        });
+
+        // reset user btclow to null
+        setTimeout(function () {
+            var queryReset = `UPDATE trader SET btclow=NULL WHERE email='${trader.email}';`;
+            connection.query(queryReset, function(error, rows, columns) {
+                if(error) {console.log(error); return;}     //  stop if you get errors
+                console.log('rows: ', rows);
+            })
+        }, 5000)
+    }
+}
+
+// ALERT USER BTC HIGH
+function alertUserBtcHigh(trader) {
+    console.log('alerting user about btc high: ', trader);
+    if(trader.twit_acc) {       // twit user
+        
+    }
+    if(trader.phone) {          // sms user
+        
+    }
+    if(trader.cool == 'Y') {    // email user
+        // prepare email
+        var mail = {
+            from: "Crypto Alert <mandybot1339@gmail.com>",
+            to: trader.email,
+            subject:  'btc price up',
+            text: "Bitcoin went up in price to your threshold of " + trader.btchigh + 'your threshold is being reset now',
+            html: "<b>Bitcoin went up in price to your threshold of " + trader.btchigh + 'your threshold is being reset now</b>',
+        }
+        // send email
+        smtpTransport.sendMail(mail, function(error, response){
+            if(error) { console.log(error); }
+            else { console.log("Message sent to: " + response.accepted);}
+        });
+        
+        // reset user btchigh to null
+        setTimeout(function () {
+            var queryReset = `UPDATE trader SET btchigh=NULL WHERE email='${trader.email}';`;
+            connection.query(queryReset, function(error, rows, columns) {
+                if(error) {console.log(error); return;}     //  stop if you get errors
+                console.log('rows: ', rows);
+            })
+        }, 5000)
+    }
+}
